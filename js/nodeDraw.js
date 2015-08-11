56cc2a3eeb48ea29a7e0601bd5f5fff95c87a9aa
unknown <mcherry@USMAYMCHERRYPC2.ra-int.com>
2015-08-11 16:52:23 -0400

    Adding python server/implementing search algorithm

--- js/nodeDraw.js
+++ js/nodeDraw.js
@@ -18,6 +18,13 @@ d3.csv("Routes.csv", function(error, links) {
   var width = 960,
       height = 500;
 
+  var currentLine = [];
+  var minWeight = 10000000;
+  var currentEdge = -1;
+
+  var settledNode = [];
+  var unsettledNodes = [];
+
   var force = d3.layout.force()
       .nodes(d3.values(nodes))
       .links(links)
@@ -95,7 +102,6 @@ d3.csv("Routes.csv", function(error, links) {
   }
 
   function color(node){
-    console.log(node);
     var i = selectedNodes.indexOf(node.index);
   
     var color = "#0F0";
@@ -103,10 +109,6 @@ d3.csv("Routes.csv", function(error, links) {
       color = "#08D";
     else if(i == 1)
       color = "#F00";
-    if(i >= 0 ){
-      console.log(i);
-      console.log(color);
-    }
     return color;
 
   }
@@ -125,219 +127,48 @@ d3.csv("Routes.csv", function(error, links) {
       }
     }
   };
-  var currentLine = [];
-  var minWeight = 10000000;
-  var currentEdge = -1;
-  function calulatePath(){
 
+  function calulatePath(){
+    //set up the nodes appropriately.
+    unsettledNodes = [];
+
+    for (var ind in nodes) {
+      if (nodes.hasOwnProperty(ind)) {
+        var curNode = nodes[ind];
+        console.log(curNode);
+        curNode.minPath = [];
+        curNode.minDistance = minWeight;
+        unsettledNodes.push(curNode);
+      }
+    };
+
+
+    if(selectedNodes.indexOf(-1) < 0){
+      // we have a path potentially!
+      console.log(selectedNodes);
+      console.log(unsettledNodes);
+      var firstNode = unsettledNodes[selectedNodes[0]];
+      var destNode = unsettledNodes[selectedNodes[1]];
+      var lastAddedNode = firstNode; 
+
+      while(lastAddedNode.index != destNode.index)
+      {
+        RelaxNeighbors(lastAddedNode);
+        var minNode = ExtractMinimum()
+        var nextEdgeWeights
+      }
+    }
   };
 
+  function relaxNeighbors(node){
+    var filteredLinks = links.filter(function(link){
+      return link.source.index = node.index;
+    });
+
+  }
+
+  function extractMinimum(set){
+
+  }
+
 });
-
-
-// (function (d3, _) {
-//   'use strict';
-//   console.log('Started!');
-
-//   var nodes = []
-
-//   $(document).ready(function() {
-//     $.ajax({
-//         type: "GET",
-//         url: "Routes.csv",
-//         dataType: "text",
-//         success: function(data) {processData(data);}
-//      });
-//   });
-
-//   var routesArray = [];
-//   function processData(allText){ 
-//     var textComingIn = "";
-//     if(allText != undefined)
-//       textComingIn = allText;
-//     var allTextLines = textComingIn.split(/\r\n|\n/);
-//     var headers = allTextLines[0].split(',');
-
-//     for (var i=1; i<allTextLines.length; i++) {
-//         var data = allTextLines[i].split(',');
-//         if (data.length == headers.length) {
-
-//             var tarr = [];
-//             for (var j=0; j<headers.length; j++) {
-//                 tarr.push(data[j]);
-//             }
-//             var dist = tarr[2]*10;
-//             console.log(dist);
-//             var route = { 
-//               src: tarr[0],
-//               dest: tarr[1], 
-//               distance: tarr[2]
-//             };
-//             routesArray.push(route);
-//         }
-//     }
-//     CreateNodes();
-//   }
-
-//   function CreateNodes() {
-//     var latestNodeIndex = 0;
-//     for(var i =0; i< routesArray.length; i++){
-//       var source = routesArray[i].src;
-//       var dest = routesArray[i].dest;
-//       var index = _.find(nodes, function(existingNode){
-//         return source == existingNode.name;
-//       });
-//       if(index == undefined){
-//         var node = {
-//           name: source,
-//           index: latestNodeIndex};
-//         nodes.push(node);
-//         console.log("Latest Node Index = ")
-//         console.log(latestNodeIndex)
-//         index = latestNodeIndex;
-//         latestNodeIndex++;
-//       }
-//       else{
-//         console.log("elseStatemetnIndex = ")
-//         console.log(index)
-//         index=index.index;
-//       }
-//       console.log(index);
-//       //Set the route's source to the index of the node now that it is known
-//       routesArray[i].source = index;
-
-//       index = _.find(nodes, function(existingNode){
-//         return dest == existingNode.name;
-//       })
-//       if(index == undefined){
-//         var node = {
-//           name: dest,
-//           index: latestNodeIndex};
-//         nodes.push(node);
-//         index = latestNodeIndex;
-//         latestNodeIndex++;
-//       }
-//       else
-//         index = index.index;
-//       // set the route's target index now that we know it.
-//       routesArray[i].target = index;
-//     }
-//     console.log(nodes);
-//     console.log(routesArray);
-//     CreateForces();
-//     DrawGraph();
-//   }
-
-//   var force;
-
-//   function CreateForces() {
-//     force = d3.layout.force()
-//     .nodes(nodes)
-//     .links(routesArray)
-//     .linkStrength(0.1)
-//     .friction(0.9)
-//     .linkDistance(function(link){
-//       return link.distance * 10;
-//     })
-//     .charge(-30)
-//     .gravity(0.1)
-//     .theta(0.8)
-//     .alpha(0.1)
-//     .start();
-//   }
-
-//   var width = 960,
-//       height = 500,
-//       centered,
-//       dotscale = 5,
-//       cities = [];
-
-//   var path = d3.geo.path();
-
-//   var svg = d3.select("#RouteGraph").append("svg")
-//       .attr("width", width)
-//       .attr("height", height);
-
-//   // Arrows
-//   svg.append("svg:defs")
-//     .append("svg:marker")
-//       .attr("id", "directed-line")
-//       .attr("viewBox", "0 0 10 10")
-//       .attr("refX", 15)
-//       .attr("refY", 1.5)
-//       .attr("markerWidth", 6)
-//       .attr("markerHeight", 6)
-//       .attr("orient", "auto")
-//     .append("svg:path")
-//       .attr("d", "M0,-5L10,0L0,5");
-
-//   svg.append("rect")
-//       .attr("class", "background")
-//       .attr("width", width)
-//       .attr("height", height)
-//       .on('click', clickMap);
-
-//   function DrawGraph(){
-//     console.log("amazeballs");
-
-//     var node = svg.selectAll("circle").data(nodes)
-//       .enter().append("circle")
-//       .attr("class", "city")
-//       .call(force.drag);
-
-//     node.append("text")
-//       .attr("dx", 12)
-//       .attr("dy", ".35em")
-//       .text(function(d) {return d.name});
-
-//     var link = svg.selectAll("path.connection").data(routesArray)
-//       .enter().append("path")
-//         .attr('class', 'connection')
-//         .attr("marker-end", "url(#directed-line)");
-
-//     console.log(routesArray);
-//     console.log(nodes);
-//     console.log(svg);
-//     console.log(force);
-//   }
-
-//   function clickMap () {
-//     cities.push(d3.mouse(this));
-//     drawCities();
-//   }
-
-//   function reset () {
-//     cities = [];
-//     svg.selectAll('circle').remove();
-//     svg.selectAll('path.connection').remove();
-//   }
-
-//   function drawCities() {
-//     console.log(svg);
-//     svg.selectAll('circle').data(nodes).enter()
-//       .append('circle')
-//         .attr('cx', function (d) { return d[0]; })
-//         .attr('cy', function (d) { return d[1]; })
-//         .attr('r', dotscale)
-//         .attr('class', 'city');
-//   }
-
-//   function drawPaths(ipath) {
-//     var paths = _.map(_.zip(ipath.slice(0,ipath.length-1), ipath.slice(1)), function (pair) {
-//       return [cities[pair[0]], cities[pair[1]]]
-//       }).slice();
-
-//     svg.selectAll('path.connection').remove();
-//     svg.selectAll('path.connection').data(paths).enter()
-//       .append('path')
-//       .attr('d', function(d) {
-//         var dx = d[1][0] - d[0][0],
-//         dy = d[1][1] - d[0][1],
-//         dr = Math.sqrt(dx * dx + dy * dy);
-//         return "M" + d[0][0] + "," + d[0][1] + "A" + dr + "," + dr + " 0 0,1 " + d[1][0] + "," + d[1][1];
-//       })
-//       .attr('class', 'connection')
-//       .attr("marker-end", "url(#directed-line)");
-//   }
-
-// })(d3, _);
