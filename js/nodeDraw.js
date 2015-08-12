164fe3cf65563e390cf3a6b32e8eb7612f8d616b
mttchrry <mttchrry@gmail.com>
2015-08-11 23:04:07 -0400

    almost there with the fastest route

--- js/nodeDraw.js
+++ js/nodeDraw.js
@@ -11,18 +11,15 @@ d3.csv("Routes.csv", function(error, links) {
           (nodes[link.target] = {name: link.target});
       link.value = +link.value;
   });
-
-  console.log(links);
-  var selectedNodes = [-1, -1];
-  console.log(selectedNodes);
+  var selectedNodes = [undefined, undefined];
   var width = 960,
       height = 500;
 
   var currentLine = [];
-  var minWeight = 10000000;
+  var maxMinWeight = 10000000;
   var currentEdge = -1;
 
-  var settledNode = [];
+  var settledNodes = [];
   var unsettledNodes = [];
 
   var force = d3.layout.force()
@@ -102,8 +99,8 @@ d3.csv("Routes.csv", function(error, links) {
   }
 
   function color(node){
-    var i = selectedNodes.indexOf(node.index);
-  
+    var i = selectedNodes.indexOf(node);
+    console.log(node);
     var color = "#0F0";
     if(i == 0)
       color = "#08D";
@@ -113,62 +110,106 @@ d3.csv("Routes.csv", function(error, links) {
 
   }
 
+  function colorLines(pathIn){
+    console.log(pathIn);
+    console.log(lastAddedNode);
+    if(lastAddedNode != undefined &&
+      lastAddedNode.minPath.indexOf(pathIn) >= 0) {
+      return "#EE0"
+    }
+    // else if(lastAddedNode != undefined && pathIn.source.index == 0){
+    //   var i = lastAddedNode.minPath.indexOf(pathIn);
+    //   console.log(i);
+    //   console.log(lastAddedNode.minPath[0]);
+    //   console.log(pathIn);
+    //   return "#000";
+    // }
+  }
+
   function selectNode(selectedNode){
-    console.log(selectedNode.name);
     // if the selected node isn't the destination, it becomes it. 
-    if (selectedNodes.indexOf(selectedNode.index) < 1){
+    if (selectedNodes.indexOf(selectedNode) < 1){
       // move the current destination to the source, and add this as destination;
       selectedNodes[0] = selectedNodes[1];
-      selectedNodes[1] = selectedNode.index;
+      selectedNodes[1] = selectedNode;
        // Update the nodes…
       svg.selectAll("circle").style("fill", color);
-      if(selectedNodes[0] >= 0){
+      if(selectedNodes[0] != undefined){
         calulatePath();
       }
+      svg.selectAll("path").style("stroke", colorLines);
     }
   };
 
+  var lastAddedNode;
+
   function calulatePath(){
     //set up the nodes appropriately.
-    unsettledNodes = [];
+    unsettledNodes = JSON.parse(JSON.stringify(nodes));
+    settledNodes = [];
 
-    for (var ind in nodes) {
-      if (nodes.hasOwnProperty(ind)) {
-        var curNode = nodes[ind];
-        console.log(curNode);
+    for (var ind in unsettledNodes) {
+      if (unsettledNodes.hasOwnProperty(ind)) {
+        var curNode = unsettledNodes[ind];
         curNode.minPath = [];
-        curNode.minDistance = minWeight;
-        unsettledNodes.push(curNode);
+        curNode.minDistance = maxMinWeight;
       }
     };
 
-
-    if(selectedNodes.indexOf(-1) < 0){
+    if(selectedNodes.indexOf(undefined) < 0){
       // we have a path potentially!
-      console.log(selectedNodes);
-      console.log(unsettledNodes);
-      var firstNode = unsettledNodes[selectedNodes[0]];
-      var destNode = unsettledNodes[selectedNodes[1]];
-      var lastAddedNode = firstNode; 
+      var firstNode = unsettledNodes[selectedNodes[0].name];
+      firstNode.minDistance = 0;
+      var destNode = unsettledNodes[selectedNodes[1].name];
+      lastAddedNode = firstNode; 
 
-      while(lastAddedNode.index != destNode.index)
+      settledNodes.push(firstNode);
+      delete unsettledNodes[selectedNodes[0].name];
+
+      while(lastAddedNode != undefined &&
+        lastAddedNode.name != destNode.name)
       {
-        RelaxNeighbors(lastAddedNode);
-        var minNode = ExtractMinimum()
-        var nextEdgeWeights
+        relaxNeighbors(lastAddedNode);
+        lastAddedNode = extractMinimum(unsettledNodes);
       }
     }
   };
 
   function relaxNeighbors(node){
     var filteredLinks = links.filter(function(link){
-      return link.source.index = node.index;
+      return link.source.index == node.index;
     });
+    filteredLinks.forEach(function(link){
+      var targetNode = unsettledNodes[link.target.name];
+      if(targetNode != undefined)
+      {
+        var thisLinkPathDist = node.minDistance+link.value;
+        if(targetNode.minDistance > thisLinkPathDist){
+          targetNode.minPath = JSON.parse(JSON.stringify(node.minPath));
+          targetNode.minPath.push(link);
+          targetNode.minDistance = thisLinkPathDist;
+        }
+      }
+    })
 
   }
 
   function extractMinimum(set){
-
+    var nextMin = maxMinWeight;
+    var nextNode = undefined;
+    for (var prop in unsettledNodes) {
+      if (unsettledNodes.hasOwnProperty(prop)) {
+        var curNode = unsettledNodes[prop];
+        if(curNode.minDistance < nextMin){
+          nextMin = curNode.minDistance;
+          nextNode = curNode;
+        }
+      }
+    };
+    if(nextNode != undefined){
+      delete unsettledNodes[nextNode.name];
+      settledNodes[nextNode.name] = nextNode;
+    }
+    return nextNode;
   }
-
 });
